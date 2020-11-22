2e8ffe05527212c45b5a66dc17e95d10c76cca71
Milica <milica.milakovic@student.etf.unibl.org>
2020-11-22 15:11:27 +0100

    Created a Task

--- Zadatak1.Demo/Program.cs
+++ Zadatak1.Demo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+
 
 namespace Zadatak1.Demo
 {
@@ -7,6 +9,17 @@ namespace Zadatak1.Demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Task task = Task.Factory.StartNew(() =>
+            {
+               int i = 0;
+               for (i = 0; i <= 10000; i++)
+               {
+               }
+               Console.WriteLine("Finished. {0} iterations", i);
+            });
+
+            task.Wait();
         }
     }
 }
