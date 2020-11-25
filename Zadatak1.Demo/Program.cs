9502a7539ea3bc2cc1583d13d686b42475600c61
Milica <milica.milakovic@student.etf.unibl.org>
2020-11-25 20:05:25 +0100

    Drugi pokusaj

--- Zadatak1.Demo/Program.cs
+++ Zadatak1.Demo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,26 +8,106 @@ namespace Zadatak1.Demo
 {
     class Program
     {
+        
         static void Main(string[] args)
-        {
+        {          
+                       
             Console.WriteLine("Hello World!");
+          
 
-            Task task = Task.Factory.StartNew(() =>
+            MyTaskScheduler mts = new MyTaskScheduler(4);
+
+            Action action1 = () =>
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        Console.WriteLine("5    " + Thread.CurrentThread.ManagedThreadId);
+                       
+                        Thread.Sleep(1000);
+                    }
+                };
+
+            Task t11 = new Task(action1);
+
+
+            //TaskFactory tf = new TaskFactory(mts);
+
+            //Task tttt = Task.Factory.StartNew(() =>
+            //{
+            //    Console.WriteLine(5);
+            //    Thread.Sleep(1000);
+            //});
+
+
+            mts.AddTask(5, t11);
+            Thread.Sleep(2000);
+
+           Action action2 = () =>
             {
-               int i = 0;
-               for (i = 0; i <= 10000; i++)
-               {
-               }
-               Console.WriteLine("Finished. {0} iterations", i);
-            });
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine("3    " + Thread.CurrentThread.ManagedThreadId);
+                    Thread.Sleep(1000);
+                }
+            };
+            Task t2 = new Task(action2);
 
-            task.Wait();
+            mts.AddTask(3, t2);
 
-            Task taskB = Task.Run(() => Thread.Sleep(2000));
-            Console.WriteLine("taskB status {0}", taskB.Status);
 
-            taskB.Wait();
-            Console.WriteLine("taskB status {0}", taskB.Status);
+            Action action3 = () =>
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine(9 + "    "+ Thread.CurrentThread.ManagedThreadId);
+                    Thread.Sleep(1000);
+                }
+            };
+            Task t3 = new Task(action3);
+
+            mts.AddTask(9, t3);
+
+
+            Action action4 = () =>
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine(2 + "    " + Thread.CurrentThread.ManagedThreadId);
+                    Thread.Sleep(1000);
+                }
+            };
+            Task t4 = new Task(action4);
+
+            mts.AddTask(2, t4);
+
+
+
+
+            Action action5 = () =>
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine(1 + "    " + Thread.CurrentThread.ManagedThreadId);
+                    Thread.Sleep(1000);
+                }
+            };
+            Task t5 = new Task(action5);
+
+            mts.AddTask(1, t5);
+
+            /*
+
+            Action action6 = () =>
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine(7);
+                    Thread.Sleep(1000);
+                }
+            };
+            Task t6 = new Task(action6);
+            mts.AddTask(7, t6);
+            */
 
 
         }
--- Zadatak1/MyScheduler.cs
+++ Zadatak1/MyScheduler.cs
@@ -6,16 +6,17 @@ using System.Threading.Tasks;
 
 namespace Zadatak1
 {
-   
-    public class MyTaskScheduler : TaskScheduler
+
+    public class MyTaskScheduler : TaskScheduler, IDisposable
     {
-         // svi taskovi 
-         private List<(int ,Task)> allTasks = new List<(int,Task)>();
+        //niti u bazenu
+        private Thread[] myThreadPool;
 
-         // taskovi koji treba da se izvrse
-         public LinkedList<Task> pendingTasks = new LinkedList<Task>(); 
+        // svi taskovi 
+        private List<(int, Task)> allTasks = new List<(int, Task)>();
 
-        
+        // taskovi koji treba da se izvrse
+        public LinkedList<Task> pendingTasks = new LinkedList<Task>();
 
         // koliko taskova moze istovremeno da se izvrsava, broj niti u thread pool-u
         private int MaxDegreeOfParallelism;
@@ -27,77 +28,109 @@ namespace Zadatak1
         {
             if (maxDegreeOfParallelism < 1) throw new Exception();
             MaxDegreeOfParallelism = maxDegreeOfParallelism;
-            RunTask();
+
+            myThreadPool = new Thread[maxDegreeOfParallelism];
+            for (int i = 0; i < maxDegreeOfParallelism; i++)
+            {
+                myThreadPool[i] = new Thread(RunTask)
+                {
+                    IsBackground = false
+                 };
+                myThreadPool[i].Start();
+                Console.WriteLine(myThreadPool[i].ManagedThreadId +" start " );
+            }
         }
-        
+
 
         // override
         protected override IEnumerable<Task> GetScheduledTasks()
         {
+            Console.WriteLine("mgfhf");
             return pendingTasks;
         }
 
-        
+
         protected override void QueueTask(Task task)
         {
-            lock (pendingTasks)
-            {
                 pendingTasks.AddLast(task);
-
-                if (currentlyRunning < MaxDegreeOfParallelism)
-                {
-                    ++currentlyRunning;
-                    RunTask();
-                }
-            }
-            
         }
 
         private void RunTask()
         {
-            ThreadPool.QueueUserWorkItem(_ =>
-          {
-              while(true)
-              {
-                  Task task;
-                  lock(pendingTasks)
-                  {
-                      if(pendingTasks.Count == 0)
-                      {
-                          --currentlyRunning;
-                          break;
-                      }
-
-                      task = pendingTasks.First.Value;
-                      pendingTasks.RemoveFirst();
-                      allTasks.RemoveAt(0);
-                  }
-
-                  base.TryExecuteTask(task);
-              }
-          });
-        }
-
-
-        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
-        {         
-            return false;
-        }
-
-
-        public void AddTask(int priority, Task task)
-        {
-            allTasks.Add((priority, task));
-            allTasks.Sort((x, y) => x.Item1.CompareTo(y.Item1));
-
-            lock(pendingTasks)
+            while (true)
             {
-                pendingTasks.Clear();
-                foreach( var a in allTasks)
+                Thread.Sleep(500);
+
+                try
                 {
-                    QueueTask(a.Item2);
+                    Task task;
+
+                    lock (pendingTasks)
+                    {
+
+                        if (pendingTasks.Count == 0)
+                        {
+                            //Console.WriteLine(" a");
+                            continue;
+                        }
+
+                        task = pendingTasks.First.Value;
+                        pendingTasks.RemoveFirst();
+                        allTasks.RemoveAt(0);
+                    }
+
+
+                    Console.WriteLine(task.Status);
+                    task.Start();
+
+                } catch (Exception e)
+                {
+                    Console.WriteLine(" ee");
+                    Console.WriteLine(e.Message);
                 }
             }
+
         }
-    }
+
+
+
+            protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+            {
+                Console.WriteLine("ne daj boze");
+                return false;
+            }
+
+
+            public void AddTask(int priority, Task task)
+            {
+                allTasks.Add((priority, task));
+                allTasks.Sort((x, y) => x.Item1.CompareTo(y.Item1));
+
+                lock (pendingTasks)
+                {
+                    pendingTasks.Clear();
+                    for (int i=0; i<allTasks.Count; i++)
+                    {
+                        //if (!allTasks[i].Item2.Status.Equals(TaskStatus.Running)) 
+                            QueueTask(allTasks[i].Item2);
+                    }
+                }
+            }
+
+
+            public static void printMethod (int priority)
+            {
+                for(int i=0; i<10; i++)
+                {
+                    Console.WriteLine(priority);
+                    Thread.Sleep(1000);
+                }
+           
+            }
+
+            public void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+        }
 }
